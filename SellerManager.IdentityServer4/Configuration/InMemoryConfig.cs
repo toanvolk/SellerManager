fae28f14ed5cd6f738ece8df7caa3014dba53fb3
toan-vo-lk <toanvolk@gmail.com>
2020-08-12 18:06:12 +0700

    identityServer-add-1

---
+++ SellerManager.IdentityServer4/Configuration/InMemoryConfig.cs
@@ -0,0 +1,12 @@
+﻿using IdentityServer4.Models;
+using System.Collections.Generic;
+
+public static class InMemoryConfig
+{
+    public static IEnumerable<IdentityResource> GetIdentityResources() =>
+      new List<IdentityResource>
+      {
+          new IdentityResources.OpenId(),
+          new IdentityResources.Profile()
+      };
+}
